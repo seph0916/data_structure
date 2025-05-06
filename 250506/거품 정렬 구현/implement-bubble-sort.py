d1595f2da0a20afed89030499e23a39c22ea7fa0
Joseph Lee <117637947+seph0916@users.noreply.github.com>
2025-05-06 16:00:42 +0900

    Title: 거품 정렬 구현, Time: 71ms, Memory: 18MB, Status: Runtime Error - Codetree

--- 250506/거품 정렬 구현/implement-bubble-sort.py
+++ 250506/거품 정렬 구현/implement-bubble-sort.py
@@ -5,4 +5,13 @@ arr = list(map(int, input().split()))
 
 
 def bubble_sort(num, ls)i:
-    
+    for i in range(n):
+        for j in range(n - 1 - i):
+            if arr[j] > arr[j+1]
+                data = arr[j]
+                arr[j] = arr[j+1]
+                arr[j+1] = data
+    return arr
+
+for d in arr:
+    print(d, end=" ")
